c40b34b8ba0793c08c745312856d61de18f1be79
Romain Petit <contact@romainpetit.com>
2020-02-26 19:38:59 +0100

    Header | Grid alignment, menu items sizing

--- wp-content/themes/shapely.1.1.2/shapely/header.php
+++ wp-content/themes/shapely.1.1.2/shapely/header.php
@@ -31,7 +31,7 @@
 		<div class="nav-container">
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 				<div class="container nav-bar">
-					<div class="row">
+					<div class="flex-center">
 						<div class="module left site-title-container">
 							<?php shapely_get_header_logo(); ?>
 						</div>
