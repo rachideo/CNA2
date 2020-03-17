1efb747fb2354ab4ea30862b8cb50d1a4959e9ce
unknown <rachid.el-mahdali@le-campus-numerique.fr>
2020-03-17 15:02:23 +0100

    Changement du transform scale de moving-letters, Hero, page Home

--- wp-content/themes/shapely.1.1.2/shapely/footer.php
+++ wp-content/themes/shapely.1.1.2/shapely/footer.php
@@ -84,7 +84,7 @@
     var ml4 = {};
     ml4.opacityIn = [0,1];
     ml4.scaleIn = [0.2, 1];
-    ml4.scaleOut = 3;
+    ml4.scaleOut = 1.2;
     ml4.durationIn = 800;
     ml4.durationOut = 600;
     ml4.delay = 500;
