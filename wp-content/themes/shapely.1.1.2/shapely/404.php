69c1acd772c864e1eea1aea98d39587976b5faf3
unknown <rachid.el-mahdali@le-campus-numerique.fr>
2020-03-25 10:46:14 +0100

    Ajout page 404, clean CSS, meilleure adaptation sur les écrans de plus de 2000px de large

--- wp-content/themes/shapely.1.1.2/shapely/404.php
+++ wp-content/themes/shapely.1.1.2/shapely/404.php
@@ -11,52 +11,19 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<section>
+                <div class="container page404">
+                    <div class="text404 row">
+                        <h1>Oops ! Erreur 404 </h1>
+                        <p>La page que vous recherchez n'existe pas.</p>
+                    </div>
+                    <a href="/">
+                        <span><b>Retourner à la page d'Accueil</b></span>
+                    </a>
+                </div>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'shapely' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'shapely' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( shapely_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'shapely' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'shapely' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
